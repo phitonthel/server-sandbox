15304c0028cccabd87f84851a82704c620d2797e
phitonthel <thephitonthel@gmail.com>
2021-04-05 17:09:25 +0700

    changed port

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const app = express()
 const cors = require('cors')
 const routes = require('./routers/index')
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('Listening on port:' + 3000);
+app.listen(PORT, () => {
+  console.log('Listening on port:' + PORT);
 });
 
 module.exports = app
